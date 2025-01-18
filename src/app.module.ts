7a3619ce825430891109769ccaebb01a0bbd5577
gabriel <gabriel.soares14563@gmail.com>
2025-01-18 10:37:54 -0300

    config: TypeORM e MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT) ?? 3306,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [],
+      synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
